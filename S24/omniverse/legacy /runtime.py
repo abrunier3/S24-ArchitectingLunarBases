bceaeafe2001c28df1e5eea00dc40978189590c4
Arthur Brunier <arthur.brunier@etu.ec-lyon.fr>
2026-03-04 17:23:51 -0500

    Delete runtime.py

--- S24/omniverse/legacy /runtime.py
+++
@@ -1,8 +0,0 @@
-from omni.isaac.kit import SimulationApp
-
-def start_omniverse(headless=False):
-    config = {
-        "headless": headless,
-        "renderer": "RayTracedLighting"
-    }
-    return SimulationApp(config)
