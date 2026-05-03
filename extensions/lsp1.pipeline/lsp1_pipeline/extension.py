ead0ee38a7e66752e0132f867cfc3bf5744d4dd5
ivmalav-a11y <ivmalav@gmail.com>
2026-05-03 17:55:13 -0500

    Update extension.py

--- extensions/lsp1.pipeline/lsp1_pipeline/extension.py
+++ extensions/lsp1.pipeline/lsp1_pipeline/extension.py
@@ -388,7 +388,7 @@ class LSP1PipelineExtension(omni.ext.IExt):
             )
 
             # Main transparency value
-            shader.CreateInput("opacity", Sdf.ValueTypeNames.Float).Set(0.01)
+            shader.CreateInput("opacity", Sdf.ValueTypeNames.Float).Set(0.05)
 
             material.CreateSurfaceOutput().ConnectToSource(
                 shader.ConnectableAPI(), "surface"
@@ -407,7 +407,7 @@ class LSP1PipelineExtension(omni.ext.IExt):
                     gprim = UsdGeom.Gprim(prim)
 
                     # This directly authors USD display opacity
-                    gprim.CreateDisplayOpacityAttr().Set([0.01])
+                    gprim.CreateDisplayOpacityAttr().Set([0.05])
 
                     # Bind material strongly
                     UsdShade.MaterialBindingAPI(prim).Bind(
