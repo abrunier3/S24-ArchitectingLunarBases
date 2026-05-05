2e7e97c3662139827ab67b9fde627513150e4db9
ivmalav-a11y <ivmalav@gmail.com>
2026-05-04 20:29:04 -0500

    Update extension.py

--- extensions/lsp1.pipeline/lsp1_pipeline/extension.py
+++ extensions/lsp1.pipeline/lsp1_pipeline/extension.py
@@ -275,9 +275,9 @@ class LSP1PipelineExtension(omni.ext.IExt):
 
             # Offset camera behind + above rover
             cam_pos = Gf.Vec3d(
-                target_pos[0] + 30.0,   # left/right offset
-                target_pos[1] - 30.0,   # forward/back offset
-                target_pos[2] + 30.0    # height
+                target_pos[0] + 120.0,   # left/right offset
+                target_pos[1] - 120.0,   # forward/back offset
+                target_pos[2] + 120.0    # height
             )
             
             xformable.AddTranslateOp().Set(cam_pos)
